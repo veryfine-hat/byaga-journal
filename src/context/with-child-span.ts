8f4fe9221e4cfd68d7e1b380b92715a28845c08b
Bryan Pease <bcp015000@gmail.com>
2024-04-23 19:07:44 -0500

    Fixed issues causing withChildSpan defered types to not map correctly

--- src/context/with-child-span.ts
+++ src/context/with-child-span.ts
@@ -22,8 +22,8 @@ export function withChildSpan (name:string) {
      * @param fn - The function to be wrapped.
      * @returns - A higher-order function.
      */
-    function higherOrderFunctionGenerator<T extends method>(fn: T) {
-        type ResolvedFunctionReturn = Promise<SafeResponse<Awaited<ReturnType<T>>>>
+    function higherOrderFunctionGenerator<Args extends unknown[], Result>(fn: (...args: Args) => Result) {
+        type ResolvedFunctionReturn = Promise<SafeResponse<Awaited<Result>>>
         /**
          * A higher-order function that creates a child span, annotates it with the given name,
          * executes the wrapped function, and handles any exceptions that occur during execution.
@@ -32,11 +32,11 @@ export function withChildSpan (name:string) {
          * @returns A promise that resolves with the result of the wrapped function,
          * or rejects with an error object if an exception occurs.
          */
-        function higherOrderFunction(...args: Parameters<T>): ResolvedFunctionReturn {
+        function higherOrderFunction(...args: Args): ResolvedFunctionReturn {
             return createSpan(async (): ResolvedFunctionReturn => {
                 annotate('name', name);
                 try {
-                    return { result: (await fn(...args)) as Awaited<ReturnType<T>> };
+                    return { result: (await fn(...args)) as Awaited<Result> };
                 } catch (error) {
                     exception(error);
                     return { error };
